6ce4680c306f58022dfe7fb4224751866091aa21
Aniruddh Balram <abalram1@umd.edu>
2023-07-11 19:20:14 +0100

    feature_in_progress: Integrated all completed components

--- frontend/src/Components/Detailed_View/DetailedView.js
+++ frontend/src/Components/Detailed_View/DetailedView.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import Datepicker from '../Datepicker';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import CardGroup from './CardGroup';
+import InventoryForm from './InventoryForm';
+import styles from './../../Styles/detailedview.module.css'
 function DetailedView() {
+  const shop = 'Panera';
   return (
-    <div style={{marginTop:200}}>DetailedView</div>
+    <Container fluid className={styles[`${shop}_detailed_view_container`]}>
+      <Row>
+        <Col xs={12} md={{span:8}}>
+          <div className = {`${styles[`${shop}_detailed_view_title`]}`}>Detailed View</div>
+        </Col>
+        <Col className = {`${styles[`${shop}_detailed_view_date_container`]}`}>
+        <Datepicker/>
+        </Col>
+      </Row>
+        
+      <Row >
+        <Col xs={6} md={6} lg={6} xl={5}>
+          <CardGroup/>
+        </Col>
+        <Col>
+          <InventoryForm/>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
