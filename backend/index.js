6e9887ee863bea1fafd22396888335864b69f6ba
Aniruddh Balram <abalram1@umd.edu>
2023-07-31 12:40:24 -0400

    Completed alpha-version of the backend

--- backend/index.js
+++ backend/index.js
@@ -1,60 +1,105 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const pool = require("./db")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// 
-app.post("/add_all_inventory", async(req, res)=> {
-    try{
-        const {date} = req.body; 
-        const sql = `INSERT INTO INVENTORY_MASTER_TIMELY (INVENTORY_ID,CREATED_DATE,LOCATION_,SHOP,DESCRIPTION,SYSTEM_PAR,PACK,SIZE,
-            MEASUREMENT,UOM,CASE_,EA,LB,OZ,BAG,GAL,TRAY,SLEEVES,ON_HAND,OPEN_ORDERS,ADJUSTED_PAR,ORDER_,TOTAL_CASE) 
-            SELECT INVENTORY_ID,'${date}' AS CREATED_DATE,LOCATION_,SHOP,DESCRIPTION,SYSTEM_PAR,PACK,SIZE,
-            MEASUREMENT,UOM,CASE_,EA,LB,OZ,BAG,GAL,TRAY,SLEEVES,ON_HAND,OPEN_ORDERS,ADJUSTED_PAR,ORDER_,TOTAL_CASE 
-            FROM INVENTORY_MASTER;`
-        const run = await pool.query(sql);    
-        res.json(run.rows[0])
+// Add all inventory timely
+app.post("/add_all_inventory/:date", async (req, res) => {
+  try {
+    const { date } = req.params;
+    const day_of_month = new Date(date).getDate();
+    const day_of_week = new Date(date).getDay();
+    const sql = `INSERT INTO INVENTORY_MASTER_TIMELY (INVENTORY_ID,CREATED_DATE,LOCATION_,SHOP_NAME,DESCRIPTION,SYSTEM_PAR,PACK,SIZE,
+            MEASUREMENT,UOM,CASE_,EA,LB,OZ,BAG,GAL,TRAY,SLEEVES,ON_HAND,OPEN_ORDERS,ADJUSTED_PAR,ADJUSTED_ORDER,ORDER_,TOTAL_CASE,
+            SHOP_ID,UNLOCK, WEEK_DATE, MONTH_DATE) SELECT INVENTORY_ID,'${date}' AS CREATED_DATE,LOCATION_,SHOP_NAME,DESCRIPTION,
+            SYSTEM_PAR,PACK,SIZE, MEASUREMENT,UOM,CASE_,EA,LB,OZ,BAG,GAL,TRAY,SLEEVES,ON_HAND,OPEN_ORDERS,ADJUSTED_PAR,ADJUSTED_ORDER,ORDER_,
+            TOTAL_CASE, SHOP_ID,UNLOCK, DATE '${date}' - ${day_of_week} + 1,DATE '${date}' - ${day_of_month} + 1
+            FROM INVENTORY_MASTER;`;
+    const run = await pool.query(sql);
+    res.json(run.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-    } catch (err) {
-        console.error(err.message);
-    }
-})
+app.get("/get_distinct_dates", async (req, res) => {
+  try {
+    //const {date} = res.body;
+    const sql = `SELECT DISTINCT TO_CHAR(CREATED_DATE::date, 'MM-DD-YYYY') AS DATE 
+                     FROM INVENTORY_MASTER_TIMELY;`;
+    const run = await pool.query(sql);
+    res.json(run.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-app.get("/get_distinct_dates",async(req, res)=>{
-    try {
-        //const {date} = res.body;
-        const sql = `SELECT DISTINCT TO_CHAR(CREATED_DATE::date, 'MM-DD-YYYY') AS DATE 
-                     FROM INVENTORY_MASTER_TIMELY;`
-        const run = await pool.query(sql);
-        res.json(run);
-    } catch(err) {
-        console.error(err.message);
-    }
-})
+app.get("/get_distinct_shops/:date/:shop", async (req, res) => {
+  try {
+    const { date, shop } = req.params;
+    const sql = `SELECT DISTINCT SHOP_ID, SHOP_NAME
+                     FROM INVENTORY_MASTER_TIMELY where CREATED_DATE = ${date} SHOP_ID = ${shop};`;
+    const run = await pool.query(sql);
+    res.json(run.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-app.get("/get_single_inventory_data/:inventory_id", async (req, res)=>{
-    try {
-        const {inventory_id} = req.params;
-        const sql = `SELECT * 
+app.get("/get_inventory_data/:date", async (req, res) => {
+  try {
+    const { date } = req.params;
+    const sql = `SELECT * FROM 
+                     INVENTORY_MASTER_TIMELY WHERE CREATED_DATE = '${date}' ORDER BY INVENTORY_ID;`;
+    const run = await pool.query(sql);
+    res.json(run.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+app.get("/get_single_inventory_data/:inventory_id", async (req, res) => {
+  try {
+    const { inventory_id } = req.params;
+    const sql = `SELECT * 
                      FROM INVENTORY_MASTER_TIMELY 
                      WHERE INVENTORY_ID = '${inventory_id}' 
-                     ORDER BY CREATED_DATE DESC;`
-        const run = await pool.query(sql);
-        res.json(run.rows[0]);
-    } catch (err) {
-        console.log(err.message);
-    }
-})
+                     ORDER BY CREATED_DATE DESC;`;
+    const run = await pool.query(sql);
+    res.json(run.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
-app.put("/update_existing_inventory/:inventory_id/:date/:case_/:ea/:lb/:bag/:sleeves/:tray/:gal/:oz/:adjusted_par/:open_orders/:on_hand/:order_/:adjusted_order/:total_case",async(req,res)=>{
-    try{
-        const {inventory_id, date,case_,lb,bag,sleeves,tray,gal,oz,adjusted_par,ea, on_hand,open_orders, order_, adjusted_order, total_case} = req.params;
-        console.log(inventory_id, date,case_,lb,bag,sleeves,tray,gal,oz,adjusted_par,ea, on_hand,open_orders, order_, adjusted_order, total_case)
-        const sql = `UPDATE INVENTORY_MASTER_TIMELY 
+app.put(
+  "/update_existing_inventory/:inventory_id",
+  async (req, res) => {
+    try {
+      const {inventory_id} = req.params;
+      const {
+        date,
+        case_,
+        lb,
+        bag,
+        sleeves,
+        tray,
+        gal,
+        oz,
+        adjusted_par,
+        ea,
+        on_hand,
+        open_orders,
+        order_,
+        adjusted_order,
+        total_case,
+        sales,
+        yield
+      } = req.body;
+      const sql = `UPDATE INVENTORY_MASTER_TIMELY 
                         SET open_orders = ${open_orders},
                             on_hand = ${on_hand},
                             adjusted_par = ${adjusted_par},
@@ -68,16 +113,19 @@ app.put("/update_existing_inventory/:inventory_id/:date/:case_/:ea/:lb/:bag/:sle
                             gal = ${gal},
                             oz = ${oz},
                             ea = ${ea},
+                            sales = ${sales},
                             total_case = ${total_case},
+                            yield = ${yield},
                             updated_date = NOW()
                             WHERE INVENTORY_ID = '${inventory_id}' AND CREATED_DATE = '${date}';
-        `
-        const run = await pool.query(sql);
-        res.json("Inventory Table was Updated")
-    } catch(err) {
-        console.log(err.message);
+        `;
+      const run = await pool.query(sql);
+      res.json("Inventory Table was Updated");
+    } catch (err) {
+      console.log(err.message);
     }
-})
-app.listen(5000, ()=>{
-    console.log("Server has started at Port 5000")
-})
+  }
+);
+app.listen(5000, () => {
+  console.log("Server has started at Port 5000");
+});
